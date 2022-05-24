43bfba9673a39a997cc519b49d240fbe105c20a7
Andressa Silva <andressaszx@gmail.com>
2022-05-24 18:08:23 -0300

    findByCursoNome

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -12,14 +12,20 @@ import br.com.alura.forum.repositories.TopicoRepository;
 
 @RestController
 public class TopicosController {
-	
+
 	@Autowired
 	private TopicoRepository topicoRepository;
-	
+
 	@RequestMapping(value = "/topicos")
-	public List<TopicoDTO> lista(){
-		List<Topico> topicos = topicoRepository.findAll();
-		return TopicoDTO.converter(topicos);
+	public List<TopicoDTO> lista(String nomeCurso) {
+		if (nomeCurso == null) {
+			List<Topico> topicos = topicoRepository.findAll();
+			return TopicoDTO.converter(topicos);
+		} else {
+			List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
+			return TopicoDTO.converter(topicos);
+		}
+
 	}
 
 }
