d9a76e4d836dea500a595c547e8909355f25dc55
Andressa Silva <andressaszx@gmail.com>
2022-05-24 22:23:34 -0300

    Delete topico method implemented

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -3,13 +3,16 @@ package br.com.alura.forum.controller;
 import java.net.URI;
 import java.util.List;
 
+import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +20,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import br.com.alura.forum.dto.DetalhesDoTopicoDTO;
 import br.com.alura.forum.dto.TopicoDTO;
+import br.com.alura.forum.form.AtualizacaoTopicoForm;
 import br.com.alura.forum.form.TopicoForm;
 import br.com.alura.forum.modelo.Topico;
 import br.com.alura.forum.repositories.CursoRepository;
@@ -28,11 +32,11 @@ public class TopicosController {
 
 	@Autowired
 	private TopicoRepository topicoRepository;
-	
+
 	@Autowired
 	private CursoRepository cursoRepository;
 
-	//@RequestMapping(value = "/topicos", method = RequestMethod.GET)
+	// @RequestMapping(value = "/topicos", method = RequestMethod.GET)
 	@GetMapping
 	public List<TopicoDTO> lista(String nomeCurso) {
 		if (nomeCurso == null) {
@@ -44,22 +48,40 @@ public class TopicosController {
 		}
 
 	}
-	
-	//@RequestMapping(value = "/topicos", method = RequestMethod.POST)
+
+	// @RequestMapping(value = "/topicos", method = RequestMethod.POST)
 	@PostMapping
-	public ResponseEntity<TopicoDTO> cadastrar(@RequestBody @Valid TopicoForm topicoForm, UriComponentsBuilder uriBuilder) {
+	@Transactional
+	public ResponseEntity<TopicoDTO> cadastrar(@RequestBody @Valid TopicoForm topicoForm,
+			UriComponentsBuilder uriBuilder) {
 		Topico topico = topicoForm.converter(cursoRepository);
 		topicoRepository.saveAndFlush(topico);
-		
+
 		URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
 		return ResponseEntity.created(uri).body(new TopicoDTO(topico));
 	}
-	
+
 	@GetMapping("/{id}")
 	public DetalhesDoTopicoDTO detalhar(@PathVariable Long id) {
 		Topico topico = topicoRepository.findById(id).get();
 		return new DetalhesDoTopicoDTO(topico);
 	}
+
+	@PutMapping("/{id}")
+	@Transactional // O Spring data diz que todo método que tiver operação de escrita, deve colocar o @Transactional
+	public ResponseEntity<TopicoDTO> atualizar(@PathVariable Long id,
+			@RequestBody @Valid AtualizacaoTopicoForm topicoForm) {
+		Topico topico = topicoForm.atualizar(id, topicoRepository);
+
+		return ResponseEntity.ok(new TopicoDTO(topico));
+
+	}
+
+	@DeleteMapping("/{id}")
+	@Transactional
+	public ResponseEntity<?> remover(@PathVariable Long id) {
+		topicoRepository.deleteById(id);
+		return ResponseEntity.ok().build();
+	}
+
 }
-
-
